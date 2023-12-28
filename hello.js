88d7ab97e8ffbecb4439aae971749e2927a24f3e
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2023-12-28 10:40:00 +0530

    Export,Import

---
+++ hello.js
@@ -0,0 +1,4 @@
+const {add,sub} = require('./math.js');
+
+console.log("Math function value", add(2,4));
+console.log("Math sub value", sub(2,4));
---
+++ math.js
@@ -0,0 +1,14 @@
+
+
+function add(a,b) {
+    return a+b;
+}
+
+function sub(a,b) {
+    return a - b;
+}
+
+module.exports = {
+    add,
+    sub,
+}
